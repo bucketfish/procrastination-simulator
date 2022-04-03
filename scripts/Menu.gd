9c0ff4c9b12ba3542d93910e1ca1b6a817bdb2a9
ffernn <hihi.fernguy@gmail.com>
2022-04-03 15:14:13 +1000

    More sfx

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -6,11 +6,17 @@ onready var fancyenv = $PostProcSwitch
 
 
 func _on_Quit_pressed():
+	$Click.play()
 	get_tree().quit()
 
 func _on_Play_pressed():
+	$Click.play()
 	if fancyenv.pressed:
 		world_environment.environment = load("res://other_resources/FancyEnvironment.tres")
 	else:
 		world_environment.environment = load("res://other_resources/PotatoEnvironment.tres")
 	visible = false
+
+
+func _on_PostProcSwitch_pressed():
+	$Click.play()
