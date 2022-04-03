c3abd7bc8ec6c28312a46c78f28522f3008194f0
ffernn <hihi.fernguy@gmail.com>
2022-04-03 14:54:33 +1000

    Sfx!

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -1,10 +1,16 @@
 extends Control
 
+onready var main = get_node("/root/Main")
+onready var world_environment = main.get_node("GameObjects/WorldEnvironment")
+onready var fancyenv = $PostProcSwitch
 
 
 func _on_Quit_pressed():
 	get_tree().quit()
 
-
 func _on_Play_pressed():
+	if fancyenv.pressed:
+		world_environment.environment = load("res://other_resources/FancyEnvironment.tres")
+	else:
+		world_environment.environment = load("res://other_resources/PotatoEnvironment.tres")
 	visible = false
